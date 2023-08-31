4fce49fa7bf740f21f6a43f789794f57256a2033
Adrian Devid Menezes Santos <adriandevid36@gmail.com>
2023-08-31 20:59:07 -0300

    asdas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthProvider } from 'react-oidc-context';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const oidcConfig = {
+    authority: "http://localhost:5028",
+    client_id: "client2",
+    redirect_uri: "http://localhost:3000",
+    response_type: "code",
+    scopes: "openid email",
+    client_secret: "client2123"
+  };
+  
+  const onSignin = () => {
+    location.href = "/";
+  };
+
+  return  (
+    <AuthProvider {...oidcConfig} onSigninCallback={onSignin}>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
 }
